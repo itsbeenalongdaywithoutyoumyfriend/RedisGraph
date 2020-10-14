0f8a05417860c2d383cfcb5953c6a11fa84575e7
beng <beng1998@yeah.net>
2020-10-14 22:36:03 +0800

    try to record every consume op

--- src/mytimer/mytimer.c
+++ src/mytimer/mytimer.c
@@ -1,5 +1,76 @@
 #include "mytimer.h"
+#include "../execution_plan/ops/op.h"
+#include "../execution_plan/ops/op_conditional_traverse.h"
 #include<time.h>
+
+Timer_mql** get_timers_mql()
+{
+	static Timer_mql* timers;
+	return &timers;
+}
+void timers_init_mql()
+{
+	Timer_mql **timers_p=get_timers_mql();
+	*timers_p=array_new(Timer_mql,64);
+}
+void timers_append_mql(OpBase *p)
+{
+    Timer_mql* timers = *get_timers_mql();
+    int len=array_len(timers);
+	int ifexist=0;
+    for(int i=0;i<len;++i)
+	{
+		if(timers[i].p==p)
+		{
+            ifexist=1;
+        }
+    }
+    if(ifexist)return;
+    Timer_mql* newrecord=rm_alloc(sizeof(Timer_mql));
+    newrecord->time_psum=0;
+    newrecord->record_sum=0;
+    newrecord->p=p;
+    timers=array_append(timers,*newrecord);
+}
+
+void add_to_timer_mql(OpBase *p,double addtime,int addrecord)
+{
+	Timer_mql* timers = *get_timers_mql();
+	int len=array_len(timers);
+	int ifexist=0;
+	for(int i=0;i<len;++i)
+	{
+		if(timers[i].p==p)
+		{
+			timers[i].time_sum+=addtime;
+            timers[i].record_sum+=addrecord;
+			ifexist=1;
+			break;
+		}
+	}
+    assert(ifexist);
+}
+
+void timers_output_CondTraverse_mql()
+{
+    FILE *fp;
+	fp=fopen("/home/qlma/customized-filter/outcount-redisgraph-mql","a+");
+    Timer_mql* timers = *get_timers_mql();
+    int len=array_len(timers);
+    for(int i=0;i<len;++i)
+	{
+        CondTraverse *op = (CondTraverse *)timers[i].p;
+        fprintf(fp,"%s %s CondTraverse time_used:%lfms,record_scanned:%d\n",
+            op->ae->operand.src,
+            op->ae->operand.dest,
+            timers[i].time_sum,
+            timers[i].record_sum
+            );
+    }
+    fclose(fp);
+        
+}
+
 clock_t simpletimer_cal_mql()
 {
 	static clock_t oldclock=0;
