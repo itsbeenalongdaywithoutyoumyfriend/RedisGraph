e2c1074f4321484bab156a06bd8d6da3f361e8b5
beng <beng1998@yeah.net>
2020-10-15 14:28:03 +0800

    another way to count record scanned

--- src/execution_plan/ops/op_conditional_traverse.c
+++ src/execution_plan/ops/op_conditional_traverse.c
@@ -166,10 +166,10 @@ static Record CondTraverseConsume(OpBase *opBase) {
 		simpletimer_start_mql();
 		_traverse(op);
 		double time_used=simpletimer_end_mql();
-		add_to_timer_mql(opBase,time_used,op->recordCount);
+		add_to_timer_mql(opBase,time_used,0);
 
 	}
-
+	add_to_timer_mql(opBase,0,1);
 	/* Get node from current column. */
 	op->r = op->records[src_id];
 	/* Populate the destination node and add it to the Record.
